fa0a8052b0b89e5a95b2d453ebf88db1e1d25614
lb <styier@qq.com>
2023-07-31 16:15:53 +0800

    feat: optmize buildx use hostNetwork

--- scripts/buildx.sh
+++ scripts/buildx.sh
@@ -13,7 +13,10 @@ mkdir -p /etc/buildkit
 touch /etc/buildkit/buildkitd.toml
 
 cat >> /etc/buildkit/buildkitd.toml <<EOF
+debug = true
 [registry."beyond.io:5000"]
+  http = true
+[registry."deploy.bocloud.k8s:5001"]
   http = true
 EOF
 
@@ -21,7 +24,7 @@ docker run --privileged --rm tonistiigi/binfmt --uninstall qemu-* && \
 	docker run --privileged --rm tonistiigi/binfmt --install all && \
 	docker buildx rm mybuilder
 
-docker buildx create --use --name mybuilder --config /etc/buildkit/buildkitd.toml --buildkitd-flags '--allow-insecure-entitlement network.host security.insecure' && \
+docker buildx create --use --name mybuilder --config /etc/buildkit/buildkitd.toml --buildkitd-flags '--allow-insecure-entitlement network.host security.insecure' --driver-opt network=host  && \
      	docker buildx inspect mybuilder --bootstrap && \
     	docker buildx ls
 
