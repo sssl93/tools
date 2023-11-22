c7d0edfc65f1df8234df567bda2a2b0352cdab74
lb <styier@qq.com>
2023-11-22 17:01:49 +0800

    fix: script of buildx

--- build-arm64.sh
+++ build-arm64.sh
@@ -9,5 +9,5 @@ go build -o=bin/go-server -ldflags "-s -w" -tags netgo -a  cmd/go-webserver/main
 CGO_ENABLED=0 go build -o=bin/go-k8s -ldflags "-s -w" -tags netgo -a  cmd/go-k8s/main.go
 go build -o=bin/fibonacci -ldflags "-s -w" -tags netgo -a  cmd/fibonacci/main.go
 
-docker build --no-cache --platform linux/${ARCH} --build-arg ARCH=arm64 -t ${REGISTRY}x-tools-${ARCH}:${VERSION} .
+docker buildx build --no-cache --platform linux/${ARCH} --build-arg ARCH=arm64 -t ${REGISTRY}x-tools-${ARCH}:${VERSION} .
 docker push ${REGISTRY}x-tools-${ARCH}:${VERSION}
